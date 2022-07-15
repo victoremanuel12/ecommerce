54f4bc475be2e28ab699c0a97f937c45557d9a23
victoremanuel12 <victoremannuel12@hotmail.com>
2022-07-15 14:24:33 -0300

    .

--- components/Products.jsx
+++ components/Products.jsx
@@ -7,7 +7,7 @@ function Products({product:{image, name,slug,price}}) {
     <section>
       <Link href={`/product/${slug.current}`}>
         <div className="product-card">
-          <img src={urlFor(image && image[0])} alt="JBL Headphone black sale" style={{width:250, height:250,backgroundColor:'#bdb9b9'}} className="product-image"/>
+          <img src={urlFor(image && image[0])} alt="JBL Headphone black sale" style={{width:250, height:250,backgroundColor:'##F4F0F1'}} className="product-image"/>
           <p className='product-name'>{name}</p>
           <p className='product-price'>R${price}</p>
         </div>
